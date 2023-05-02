1cd6958a30150be427fb6c6ef86ee325c335c918
Buraiannn <brianjosephsunaz@gmail.com>
2023-05-02 20:46:18 +0800

    initialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,10 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
-
 const routes: Routes = [
-  {path: 'Home', component: HomeComponent},
-  {path: 'About', component: AboutComponent},
-  {path:'Contact', component: ContactComponent}
-
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent }
 ];
 
 @NgModule({
